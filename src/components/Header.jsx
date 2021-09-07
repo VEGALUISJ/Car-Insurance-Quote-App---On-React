d8aa3024178e598e1d4a4b2ba4b4f757b94f2c0f
VEGALUISJ <luisvega.jlvv@gmail.com>
2021-09-07 12:13:58 -0400

    added styled components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const HeaderContainer = styled.header`
+    background-color: #26DACA;
+    padding: 10px;
+    font-weight: bold;
+    color: #FFFFFF
+`;
+
+const HeaderText = styled.h1`
+    font-size: 2rem;
+    margin: 0;
+    font-family: 'Slabo 27px', serif;
+    text-align: center;
+`
 
 const Header = ({tittle}) => {
     return (
-        <header>
-            <h1>{ tittle }</h1>
-        </header>
+        <HeaderContainer>
+            <HeaderText>{ tittle }</HeaderText>
+        </HeaderContainer>
     );
 }
 
