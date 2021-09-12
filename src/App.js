fed9ba6307bd83f0530d5157b9dd706bd891c030
VEGALUISJ <luisvega.jlvv@gmail.com>
2021-09-12 00:01:33 -0400

    adding styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from 'react'
 import Header from "./components/Header"
+import Form from "./components/Form"
+
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const FromContainer = styled.div`
+  background-color: #FFF;
+  padding: 3rem;
+`;
 
 function App() {
   return (
-    <Header 
-      tittle = 'Car Insurance Quote App'
-    />
+    <Container>
+      <Header 
+        tittle = 'Car Insurance Quote App'
+      />
+
+    <FromContainer>
+      <Form/>
+    </FromContainer>
+
+    </Container>
   );
 }
 
